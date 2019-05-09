f1404882dceceac4258d909ba0ebf765cdc9c6fd
Caballero93 <soicstefan@yahoo.com>
2019-05-09 22:21:11 +0200

    Problem 1 solved with C

--- C/Problem1/Problem1.c
+++ C/Problem1/Problem1.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-    printf("HELLO WORLD!");
+    int sum = 0;
+    // find sum of all the number up to 1000, which are divisible with 3 or 5
+    for(int i = 0; i < 1000; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            sum += i;
+        }
+    }
+    printf("Wanted sum is equal to: %d.", sum);
+
 
     return 0;
 }
